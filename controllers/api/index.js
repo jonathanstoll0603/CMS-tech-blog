649eb67da7b17509a557c7523dfb44fae59f5678
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-04-06 23:52:53 -0400

    created routing index.js file

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const blogRoutes = require('./blogRoutes');
+const userRoutes = require('./userRoutes');
+
+router.use('/blog', blogRoutes);
+router.use('/user', userRoutes);
+
+module.exports = router;
