36a744489530fa75cf44aa35c110b305ecd0cdc1
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-04-07 00:29:56 -0400

    added a create post route and started editPost route to edit posts

---
+++ public/js/createPost.js
@@ -0,0 +1,37 @@
+const createForm = document.querySelector('.create-form');
+const postTitle = document.querySelector('#create-title');
+const postContent = document.querySelector('#create-content');
+
+// event handler to redirect to 
+createForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const postData = {
+        title: postTitle.value.trim(),
+        content: postContent.value.trim()
+    };
+
+    if (postData.title && postData.content) {
+        createPostHandler(postData.title, postData.content);
+    } else {
+        alert("Post unsuccessful. Ensure all necessary fields are filled in and try again.");
+        return;
+    }
+});
+
+async function createPostHandler(title, content) {
+
+    await fetch('/api/blog/', {
+        method: 'POST',
+        body: JSON.stringify({title, content}),
+        headers: { 'Content-Type': 'application/json' },
+    }).then((response) => {
+
+        console.log(response);
+        
+        document.location.replace('/dashboard');
+
+    }).catch((err) => {
+        if (err) throw err;
+    });
+};
