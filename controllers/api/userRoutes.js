11844bc9ddf6c32bc62740bd3b8bd0bac514bea8
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-04-07 00:30:24 -0400

    fixed minor bugs in user and home routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -7,7 +7,7 @@ router.post('/', async (req, res) => {
         const userData = await User.create(req.body);
 
         req.session.save(() => {
-            req.session.user_id = userData.user_id;
+            req.session.user_id = userData.id;
             req.session.logged_in = true;
             res.status(200).json(userData);
         });
@@ -41,7 +41,7 @@ router.post('/login', async (req, res) => {
 
         // Save the user's session once logged in
         req.session.save(() => {
-            req.session.user_id = userData.user_id;
+            req.session.user_id = userData.id;
             req.session.logged_in = true;
             
             res.json({ user: userData, message: 'You are now logged in!' });
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,22 +2,22 @@ const router = require('express').Router();
 const { User, Post } = require('../models');
 const withAuth = require('../utils/auth');
 
+//GET route for the home page
 router.get('/', withAuth, async (req, res) => {
     try {
         // Find all posts from database
         const postData = await Post.findAll({
             include: {
-                model: Post,
-                attributes: ['title', 'content', 'user_id'],
+                model: User,
             },
         });
 
-        // 
+        // store simplified json object 
         const posts = postData.map((post) => post.get({ plain: true }));
-        console.log(posts);
 
         res.render('homepage', {
-            posts
+            posts,
+            logged_in: req.session.logged_in
         });
 
     } catch (err) {
@@ -25,15 +25,82 @@ router.get('/', withAuth, async (req, res) => {
     }
 });
 
-router.get('/login', (req, res) => {
-    // If user is logged in, redirect to the homepage screen
-    if (req.session.logged_in) {
-        res.redirect('homepage');
-        return;
-    }
+// GET route for the dashboard page
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        // find the current user's dashboard
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Post }],
+        });
+        // store simplified json object 
+        const user = userData.get({ plain: true });
 
-    // if user not logged in, route to the login screen
-    res.render('login');
+        // render dashboard page and include everything in user object
+        res.render('dashboard', {
+            ...user,
+            logged_in: true
+        });
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET route for a specific user thread page
+router.get('/thread/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: {
+                model: User,
+                exclude: {
+                    attributes: ['password']
+                },
+            },
+        });
+        console.log(postData);
+    
+        const threadInfo = postData.map(thread => thread.get({ plain: true }));
+        res.json(threadInfo);
+    
+        res.render('thread', threadInfo);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/create', withAuth, (req, res) => {
+    try {
+        res.render('create');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+// GET route for signup page
+router.get('/signup', (req, res) => {
+    try {
+      res.render('signup');
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
+
+// GET route for the login page
+router.get('/login', (req, res) => {
+    try {
+        // If user is logged in, redirect to the homepage screen
+        if (req.session.logged_in) {
+            res.redirect('profile');
+            return;
+        }
+
+        // if user not logged in, route to the login screen
+        res.render('login');
+    } catch (err) {
+        res.status(500).json(err);
+    }
 })
 
 module.exports = router;
