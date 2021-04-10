331a4d8a92806e364160cb92df3d8f73904e8097
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-04-09 20:57:32 -0400

    added reply model

---
+++ models/Reply.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const User = require('./users');
+
+class Reply extends Model {};
+
+Reply.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        content: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'Reply'
+    },
+);
+
+module.exports = Reply;
--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./Users');
 const Post = require('./Posts');
+const Reply = require('./Reply');
 
 User.hasMany(Post, {
     foreignKey: 'user_id'
@@ -9,4 +10,21 @@ User.hasMany(Post, {
     foreignKey: 'user_id'
   });
 
-module.exports = { User, Post };
+  User.hasMany(Reply, {
+    foreignKey: 'user_id'
+  });
+
+  Reply.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+
+  Post.hasMany(Reply, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+  });
+
+  Reply.belongsTo(Post, {
+    foreignKey: 'post_id'
+  });
+
+module.exports = { User, Post, Reply };
