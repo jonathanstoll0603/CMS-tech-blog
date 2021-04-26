28e3333e805ee73faa990cacc52b271d9a5dce9e
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-04-25 21:43:43 -0400

    removed unnecessary file paths

--- models/Posts.js
+++ models/Posts.js
@@ -1,6 +1,5 @@
-const { Model, Datatypes, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-const User = require('./users');
 
 class Post extends Model {};
 
